b5ada9c45c2983e9af39bb4cc7b7969d06a529c5
weverseshopminemine <jiminp3414853@gmail.com>
2024-09-13 22:04:24 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+
+        0: {
+            slidesPerView: 1
+        },
+        1024: {
+            slidesPerView: 2
+        }
+
+    }
+});
